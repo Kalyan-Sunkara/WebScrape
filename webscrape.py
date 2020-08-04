021e32ecbc3e4fdfa2f31dd46544bccaaf039b24
Kalyan Sunkara <63612268+Kalyan-Sunkara@users.noreply.github.com>
2020-08-04 15:17:37 -0700

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -3,13 +3,26 @@ import requests
 import bs4
 from googlesearch import *
 
+#input any song here, can make it user input if you want
 query = "Water by G Lyrics"
+
+#website that will be searched
 website = ''
+
+#gives top result in google search(genius lyrics)
 for j in search(query, tld="co.in", num=1, stop=1, pause=2):
     website = j
+    #opens webpage
     webbrowser.open(j)
+  
 result = requests.get(website)
+
+#parses html
 soup = bs4.BeautifulSoup(result.text,'lxml')
+
+#finds paragraph tag that contains the lyrics
 lyrics = soup.select('p')
+
+#prints the lyrics
 print(lyrics[0].getText())
-# webbrowser.open('https://soundcloud.com')
+
